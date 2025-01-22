72261b4212bc13da28df892389b1c64453306aaa
Muhammed Shehzad <142075133+muhammedshehzad@users.noreply.github.com>
2025-01-22 11:33:17 +0530

    Update homework_details.dart

--- lib/drawer_screens/homework_details.dart
+++ lib/drawer_screens/homework_details.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
-import '../pdf_converter/pdf.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:cloudinary/cloudinary.dart';
+import 'package:http/http.dart' as http;
 
 class HomeworkDetails extends StatefulWidget {
   final String docId;
@@ -15,9 +19,86 @@ class HomeworkDetails extends StatefulWidget {
 
 class _HomeworkDetailsState extends State<HomeworkDetails> {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  String fileName = '';
+  List<XFile> pickedFiles = [];
+  String? imageurl;
 
   Future<DocumentSnapshot<Map<String, dynamic>>> fetchDetails() async {
     return firestore.collection('homeworks').doc(widget.docId).get();
+  }
+
+  Future<List<XFile>> insertFiles(BuildContext context) async {
+    final picker = ImagePicker();
+    final List<XFile>? selectedFiles = await picker.pickMultiImage();
+    if (selectedFiles != null && selectedFiles.isNotEmpty) {
+      return selectedFiles;
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('No images selected')),
+      );
+      return [];
+    }
+  }
+
+  Future<void> _uploadImage(String docId) async {
+    final url = Uri.parse('https://api.cloudinary.com/v1_1/dfcehequr/upload');
+    final request = http.MultipartRequest('POST', url)
+      ..fields['upload_preset'] = 'images'
+      ..files.add(await http.MultipartFile.fromPath('file', imageurl!));
+    final response = await request.send();
+
+    if (response.statusCode == 200) {
+      final responseData = await response.stream.toBytes();
+      final responseString = String.fromCharCodes(responseData);
+      final jsonMap = jsonDecode(responseString);
+
+      setState(() {
+        imageurl = jsonMap['url'];
+      });
+
+      final homeworkDoc =
+          await firestore.collection('homeworks').doc(docId).get();
+      List<String> currentImageUrls = [];
+      if (homeworkDoc.exists) {
+        final data = homeworkDoc.data()!;
+        var existingUrls = data['imageurl'];
+        if (existingUrls is List) {
+          currentImageUrls = List<String>.from(existingUrls);
+        } else if (existingUrls is String) {
+          currentImageUrls = [existingUrls];
+        }
+      }
+
+      currentImageUrls.add(imageurl!);
+
+      await firestore.collection('homeworks').doc(docId).update({
+        'imageurl': currentImageUrls,
+      });
+    } else {
+      print("Failed to upload image. Status code: ${response.statusCode}");
+    }
+  }
+
+  Future<void> addNewImageToList() async {
+    List<XFile> newFiles = await insertFiles(context);
+    if (newFiles.isNotEmpty) {
+      for (var file in newFiles) {
+        imageurl = file.path;
+
+        if (pickedFiles.isNotEmpty &&
+            pickedFiles.any((item) => item.path == imageurl)) {
+          return;
+        }
+
+        await _uploadImage(widget.docId);
+
+        if (imageurl != null) {
+          setState(() {
+            pickedFiles.add(XFile(imageurl!));
+          });
+        }
+      }
+    }
   }
 
   Widget build(BuildContext context) {
@@ -46,7 +127,51 @@ class _HomeworkDetailsState extends State<HomeworkDetails> {
             );
           }
 
+          List<String> imageList = [];
           final data = snapshot.data!.data()!;
+          var imageUrls = data['imageurl'];
+          if (imageUrls is List) {
+            imageList = imageUrls.whereType<String>().toList();
+          } else if (imageUrls is String) {
+            imageList = [imageUrls];
+          }
+
+          List<String> allImageUrls = [];
+
+          allImageUrls.addAll(imageList);
+
+          for (var file in pickedFiles) {
+            if (file.path.startsWith('http')) {
+              if (!allImageUrls.contains(file.path)) {
+                allImageUrls.add(file.path);
+              }
+            } else {
+              if (!allImageUrls.contains(file.path)) {
+                allImageUrls.add(file.path);
+              }
+            }
+          }
+          Future<void> deleteImageFromFirestore(String imageUrl) async {
+            try {
+              final docRef = FirebaseFirestore.instance
+                  .collection('homeworks')
+                  .doc(widget.docId);
+
+              final docSnapshot = await docRef.get();
+              final data = docSnapshot.data();
+              if (data != null && data['imageurl'] != null) {
+                List<dynamic> imageUrls = List.from(data['imageurl']);
+
+                imageUrls.remove(imageUrl);
+
+                await docRef.update({
+                  'imageurl': imageUrls,
+                });
+              }
+            } catch (e) {
+              print("Error deleting image: $e");
+            }
+          }
 
           final sections = [
             [
@@ -62,6 +187,102 @@ class _HomeworkDetailsState extends State<HomeworkDetails> {
               CustomRow2("Status", data['status'] ?? 'N/A'),
               CustomRow2("AssignedBy", data['assignedby'] ?? 'N/A'),
               CustomRow2("Estimated Time", data['estimatedtime'] ?? 'N/A'),
+              SizedBox(
+                height: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Upload Image attachments:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 10),
+                      Wrap(
+                        spacing: 12,
+                        runSpacing: 12,
+                        children: [
+                          if (allImageUrls.isNotEmpty)
+                            ...allImageUrls.map((url) {
+                              return Stack(
+                                clipBehavior: Clip.none,
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(8),
+                                    child: url.startsWith('http')
+                                        ? Image.network(
+                                            url,
+                                            height: 100,
+                                            width: 100,
+                                            fit: BoxFit.cover,
+                                          )
+                                        : Image.file(
+                                            File(url),
+                                            height: 100,
+                                            width: 100,
+                                            fit: BoxFit.cover,
+                                          ),
+                                  ),
+                                  Positioned(
+                                    top: -10,
+                                    right: -10,
+                                    child: IconButton(
+                                      onPressed: () async {
+                                        String imageUrlToDelete = url;
+                                        await deleteImageFromFirestore(
+                                            imageUrlToDelete);
+
+                                        setState(() {
+                                          allImageUrls.remove(imageUrlToDelete);
+                                        });
+                                      },
+                                      icon: Icon(
+                                        Icons.delete,
+                                        color: Colors.redAccent.shade400,
+                                      ),
+                                      tooltip: 'Delete Image',
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }).toList(),
+                          GestureDetector(
+                            onTap: addNewImageToList,
+                            child: Container(
+                              height: 100,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.blueGrey.shade400),
+                                borderRadius: BorderRadius.circular(8),
+                                color: Colors.grey.shade200,
+                              ),
+                              child: Icon(
+                                Icons.add_a_photo,
+                                size: 40,
+                                color: Colors.blueGrey.shade400,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
               CustomRow2("Description", data['description'] ?? 'N/A'),
               SizedBox(height: 16),
               Text(
@@ -79,9 +300,9 @@ class _HomeworkDetailsState extends State<HomeworkDetails> {
                 backgroundColor: Colors.grey[300],
                 color: Colors.green,
               ),
-              SizedBox(height: 16),
             ],
           ];
+
           return ListView.builder(
             padding: const EdgeInsets.all(8.0),
             itemCount: sections.length + 1,
